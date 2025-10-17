803c3a60a0ec448b929ffaf69e7f2e398bd7bf1b
uoftWANGTIANMING <tianming.wang@mail.utoronto.ca>
2025-10-17 12:27:55 -0400

    feat(core): make BreedNotFoundException checked exception

---
+++ Downloads/dog-breed-fetcher-main/src/main/java/dogapi/BreedFetcher.java
@@ -0,0 +1,24 @@
+package dogapi;
+
+import java.util.List;
+
+/**
+ * Interface for the service of getting sub breeds of a given dog breed.
+ */
+public interface BreedFetcher {
+
+    /**
+     * Fetch the list of sub breeds for the given breed.
+     * @param breed the breed to fetch sub breeds for
+     * @return list of sub breeds for the given breed
+     * @throws BreedNotFoundException if the breed does not exist
+     */
+    List<String> getSubBreeds(String breed) throws BreedNotFoundException;
+
+
+    class BreedNotFoundException extends Exception {
+        public BreedNotFoundException(String breed) {
+            super("Breed not found: " + breed);
+        }
+    }
+}
---
+++ Downloads/dog-breed-fetcher-main/src/main/java/dogapi/Main.java
@@ -0,0 +1,34 @@
+package dogapi;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String breed = "hound";
+        BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
+        int result = getNumberOfSubBreeds(breed, breedFetcher);
+        System.out.println(breed + " has " + result + " sub breeds");
+
+        breed = "cat";
+        result = getNumberOfSubBreeds(breed, breedFetcher);
+        System.out.println(breed + " has " + result + " sub breeds");
+    }
+
+    /**
+     * Return the number of sub breeds that the given dog breed has according to the
+     * provided fetcher.
+     * @param breed the name of the dog breed
+     * @param breedFetcher the breedFetcher to use
+     * @return the number of sub breeds. Zero should be returned if there are no sub breeds
+     * returned by the fetcher
+     */
+    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
+        try {
+            List<String> subBreeds = breedFetcher.getSubBreeds(breed);
+            return subBreeds.size();
+        } catch (BreedNotFoundException e) {
+            return 0;
+        }
+    }
+}
